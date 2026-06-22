12b663f13969a6dbc2b541867864c5816c91cb31
nsarver00 <sarvern1@msu.edu>
2026-06-22 10:44:52 -0400

    Update chiller_app.py

--- chiller_app.py
+++ chiller_app.py
@@ -1,29 +1,54 @@
+#User input and create chillers dictionaries
 import sys
 import pandas as pd
-from itertools import combinations
+from itertools import combinations 
 import numpy as np
-import streamlit as st
 
 st.title("Chiller Plant Optimizer")
 
-building_load = st.number_input("Building Load in Tons?")
 costperkwh = st.number_input("Cost per kWh?")
 
-def optimize_chillers(building_load, chillers):
+#Room cooling load calculator
+def room_loads(cfm,t_supply,t_return):
+    delta_t = t_return - t_supply
+    btu = delta_t * 1.085 * cfm
+    tons = btu / 12000
+    return btu,tons
+
+def building_load_calc(rooms):
+    room_btu_list = []
+    for room in rooms:
+        btu,tons = room_loads(room["cfm"],room["t_supply"],room["t_return"])
+        room_btu_list.append({
+            "name":room["name"],
+            "btu":btu,
+            "tons":tons
+        })
+    building_load_tons = sum(room["tons"] for room in room_btu_list)
+    return building_load_tons, room_btu_list
+    
+#Chiller Optimizer
+def optimize_chillers(building_load_tons,chillers):
     best_kw = 10000000
     best_group = None
+
+    #Creates a group for each in range
     for r in range(1, len(chillers) + 1):
+        #for each group reset tons and kw
         for group in combinations(chillers, r):
-            total_tons = 0
-            total_kw = 0
-            for chiller in group:
-                total_tons = total_tons + chiller["tons"]
-                kw = chiller["tons"] * chiller["eff"]
-                total_kw = kw + total_kw
-            if total_tons >= building_load:
+            total_tons = sum(chiller["tons"] for chiller in group)
+            if total_tons >= building_load_tons:
+                percent_load = building_load_tons / total_tons
+                kw_per_ton = iplv_lookup(percent_load)
+                total_kw = 0
+                for chiller in group:
+                    load_chiller = chiller["tons"] * percent_load
+                    kw_chiller = load_chiller * kw_per_ton
+                    total_kw = total_kw + kw_chiller
                 if total_kw < best_kw:
                     best_kw = total_kw
                     best_group = group
+                
     return best_kw, best_group
 
 def redundancy_check(best_group, building_load):
@@ -35,12 +60,12 @@ def redundancy_check(best_group, building_load):
     else:
         st.warning("N+1 REDUNDANCY CHECK FAILED")
 
-def calculate(best_group, best_kw, costperkwh, building_load):
+def calculate(best_group,best_kw,costperkwh,building_load_tons):
     total_tons = sum(chiller["tons"] for chiller in best_group)
     daily_cost = best_kw * costperkwh * 24
     monthly_cost = daily_cost * 30
-    utilization = (building_load / total_tons) * 100
-    return total_tons, daily_cost, monthly_cost, utilization
+    utilization = (building_load_tons / total_tons) * 100
+    return total_tons,daily_cost,monthly_cost,utilization
 
 def system_flags(total_tons, building_load):
     if total_tons > building_load:
@@ -50,9 +75,24 @@ def system_flags(total_tons, building_load):
     else:
         st.info("Building load perfectly matched")
 
+def iplv_lookup(load_pct):
+    iplv_table = {
+        0.25: 0.72,
+        0.50: 0.60,
+        0.75: 0.52,
+        1.00: 0.58
+    }
+    closest_load = min(
+        iplv_table.keys(),
+        key=lambda x: abs(x - load_pct)
+    )
+
+    return iplv_table[closest_load]
+
 def print_all(best_group, best_kw, total_tons, daily_cost, monthly_cost, utilization):
-    for chiller in best_group:
-        st.write("Running:", chiller["name"], "-->", chiller["tons"], "tons ,", chiller["eff"], "kW/ton")
+    st.write("Building Load -->",round(tons,2),"tons")
+	for chiller in best_group:
+        	st.write("Running:", chiller["name"], "-->", chiller["tons"], "tons ,", chiller["eff"], "kW/ton")
     st.write(len(best_group), "Chillers Running")
     st.write("Total kW -->", round(best_kw, 2), "kW")
     st.write("Chiller Load -->", total_tons, "Tons")
@@ -60,41 +100,47 @@ def print_all(best_group, best_kw, total_tons, daily_cost, monthly_cost, utiliza
     st.write("Monthly Cost --> $", round(monthly_cost, 2))
     st.write("Utilization (%) -->", round(utilization, 2), "%")
 
-# Load CSV
-df = pd.read_csv("Chiller_csv_1.csv")
-df.columns = df.columns.str.strip()
-chillers = df.to_dict(orient="records")
 
-best_kw, best_group = optimize_chillers(building_load, chillers)
-
+df_chillers = pd.read_csv(r"C:\Users\nsarv\Downloads\Chiller_csv_1.csv")
+df_rooms = pd.read_csv(r"C:\Users\nsarv\Downloads\data_center_rooms_practice.csv")
+df_chillers.columns = df_chillers.columns.str.strip()
+df_rooms.columns = df_rooms.columns.str.strip()
+chillers = df_chillers.to_dict(orient="records")
+rooms = df_rooms.to_dict(orient="records")
+building_load_tons,room_btu_list = building_load_calc(rooms)
+best_kw,best_group = optimize_chillers(building_load_tons,chillers)
 if best_group is None:
-    st.error("COOLING REQUIREMENT EXCEEDS CHILLER CAPACITY")
-else:
-    total_tons, daily_cost, monthly_cost, utilization = calculate(best_group, best_kw, costperkwh, building_load)
-    redundancy_check(best_group, building_load)
-    system_flags(total_tons, building_load)
-    print_all(best_group, best_kw, total_tons, daily_cost, monthly_cost, utilization)
-
-    results = []
+	st.error("COOLING REQUIREMENT EXCEEDS CHILLER CAPACITY")
+else:             
+        total_tons,daily_cost,monthly_cost,utilization = calculate(best_group,best_kw,costperkwh,building_load_tons)
+	redundancy_check(best_group,building_load_tons)
+        system_flags(total_tons,building_load_tons)
+        print_all(best_group,best_kw,total_tons,daily_cost,monthly_cost,utilization,building_load_tons)
+    #Creates results and dataframe for output csv file
+    results = []  
     for chiller in best_group:
         kw_chiller = chiller["tons"] * chiller["eff"]
-        percent_load = chiller["tons"] / total_tons
+        percent_of_load = chiller["tons"] / total_tons
         cost_chiller = kw_chiller * 24 * costperkwh
-        results.append({"name": chiller["name"],
-                        "eff": chiller["eff"],
-                        "tons": chiller["tons"],
-                        "kw_usage": kw_chiller,
-                        "percent_load": percent_load,
-                        "daily_cost": cost_chiller})
+        results.append({"name":chiller["name"],
+                        "eff":chiller["eff"],"tons":chiller["tons"],
+                        "kw_usage":kw_chiller,
+                        "percent_load":percent_of_load,
+                        "daily_cost":cost_chiller}
+            )
+    total_kw = sum(r["kw_usage"] for r in results)
+    total_cost = sum(r["daily_cost"] for r in results)
+    total_percent = sum(r["percent_load"] for r in results)
+    
+    results.append({"name":"Total",
+                    "eff":(np.mean([chiller["eff"] for chiller in best_group])),
+                    "tons":(np.mean([chiller["tons"] for chiller in best_group])),
+                    "total_kw_usage":total_kw,
+                    "total_percent":total_percent,
+                    "total_daily_cost":total_cost
+                   })
 
-    results.append({"name": "Total",
-                    "eff": round(np.mean([chiller["eff"] for chiller in best_group]),2),
-                    "tons": round(np.mean([chiller["tons"] for chiller in best_group]),2),
-                    "kw_usage": sum(r["kw_usage"] for r in results),
-                    "percent_load": round(np.mean([r["percent_load"] for r in results]), 2),
-                    "daily_cost": sum(r["daily_cost"] for r in results)})
-
-    df_results = pd.DataFrame(results)
+   df_results = pd.DataFrame(results)
     st.dataframe(df_results)
     csv = df_results.to_csv(index=False)
     st.download_button("Download Results CSV", csv, "chiller_results.csv")
